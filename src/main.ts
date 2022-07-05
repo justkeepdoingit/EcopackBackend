d000b0da9b6691c08aa93a05c461cedd6e2e6fa1
Jores Amancio <amanciojoris23@gmail.com>
2022-07-05 16:49:17 +0800

    Added Feater

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors({})
+
+  const whitelist = ['http://localhost:4200', 'http://example2.com'];
+
+  app.enableCors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if(whitelist.includes(origin))
+        return callback(null, true)
+  
+        callback(new Error('Not allowed by CORS'));
+    }
+  })
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
   // npm config set strict-ssl false
