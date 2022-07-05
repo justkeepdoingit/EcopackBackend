363ca211e2b23740209f860ed290fa8811bb219c
Jores Amancio <amanciojoris23@gmail.com>
2022-07-05 13:47:17 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    credentials: true,
-  })
+  app.enableCors({})
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
 }
